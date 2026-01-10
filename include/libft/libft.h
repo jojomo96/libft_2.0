e402c818b114e39a40984a6d29775dcafd00e033
Johannes Moritz <johannes.moritz@stackit.cloud>
2026-01-10 10:01:32 +0100

    add ft_strncmp function for string comparison with length limit

--- include/libft/libft.h
+++ include/libft/libft.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 int ft_strcmp(const char *s1, const char *s2);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strchr(const char *s, int c);
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
---
+++ src/ft_strncmp.c
@@ -0,0 +1,24 @@
+#include "libft/libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	c1;
+	unsigned char	c2;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		c1 = (unsigned char)s1[i];
+		c2 = (unsigned char)s2[i];
+		if (c1 != c2)
+			return ((int)c1 - (int)c2);
+		if (c1 == '\0')
+			return (0);
+		++i;
+	}
+	return (0);
+}
+
